543c6d2e6fc8066ca3f29d653835016a8de76ba5
Jaey0un <jeonjaeyoun4@gmail.com>
2025-08-22 13:44:12 +0800

    Level-2: Add in-memory task list with add/list commands

--- src/main/java/Mang.java
+++ src/main/java/Mang.java
@@ -3,11 +3,14 @@ import java.util.Scanner;
 public class Mang {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int count = 0;
 
         System.out.println("____________________________________________________________");
         System.out.println(" Hello! I'm Mang, your friendly neighborhood chatbot!");
         System.out.println(" What can I do for you?");
         System.out.println(" Type 'bye' whenever you want to end our chat.");
+        System.out.println("____________________________________________________________");
 
         while (true) {
             String input = sc.nextLine();
@@ -17,10 +20,25 @@ public class Mang {
                 System.out.println(" Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if (input.equals("list")){
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < count; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                if (count < 100) {
+                    tasks[count] = input;
+                    count++;
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" added: " + input);
+                    System.out.println("____________________________________________________________");
+                } else {
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" Sorry, I can only store up to 100 items.");
+                    System.out.println("____________________________________________________________");
+                }
             }
-            System.out.println("____________________________________________________________");
-            System.out.println(" " + input);
-            System.out.println("____________________________________________________________");
         }
 
         sc.close();
