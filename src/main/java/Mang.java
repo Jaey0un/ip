6b697756395caf9619efdc06b65e3d96cf1ee1ee
Jaey0un <jeonjaeyoun4@gmail.com>
2025-08-22 13:16:38 +0800

    Level-1: Add echo functionality to Mang chatbot

--- src/main/java/Mang.java
+++ src/main/java/Mang.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class Mang {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Mang");
+        System.out.println(" Hello! I'm Mang, your friendly neighborhood chatbot!");
         System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println(" Type 'bye' whenever you want to end our chat.");
+
+        while (true) {
+            String input = sc.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + input);
+            System.out.println("____________________________________________________________");
+        }
+
+        sc.close();
+
     }
 }
